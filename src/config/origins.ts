40fdcbfe01784389a985dbb89abd4c64369dca90
SK66613 <cyberian13@icloud.com>
2026-02-19 16:14:59 +0300

    Update origins.ts

--- src/config/origins.ts
+++ src/config/origins.ts
@@ -3,6 +3,7 @@ export const ALLOW_ORIGINS = [
   "https://mini.salesgenius.ru",
   "https://blocks.salesgenius.ru",
   "https://ru.salesgenius.ru",
+  "https://ru.cifrovichkoff.ru",
   "https://apps.salesgenius.ru",
   "https://web.telegram.org",
   "https://web.telegram.org/k",
