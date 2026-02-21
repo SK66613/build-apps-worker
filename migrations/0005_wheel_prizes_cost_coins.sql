da53099c8fd51104b5b767b4cc914179221fd947
SK66613 <cyberian13@icloud.com>
2026-02-21 17:09:23 +0300

    Update 0005_wheel_prizes_cost_coins.sql

--- migrations/0005_wheel_prizes_cost_coins.sql
+++ migrations/0005_wheel_prizes_cost_coins.sql
@@ -1,6 +1,2 @@
 -- 0005_wheel_prizes_cost_coins.sql
 ALTER TABLE wheel_prizes ADD COLUMN cost_coins INTEGER;
-
--- (опционально) если у тебя часто грузятся призы по app_public_id
-CREATE INDEX IF NOT EXISTS idx_wheel_prizes_app_code
-ON wheel_prizes(app_public_id, code);
